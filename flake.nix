b6d67a303ac272ebd4fbcfa42d7d06b905b3ac5f
Abel Jimenez <abel.jimenez@rekovar.com>
2024-06-25 11:23:57 -0700

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,26 @@
 {
-  description = "A flake for Parasoft C++test";
+  description = "Parasoft CppTestCli package using Nix flakes";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = nixpkgs.lib.mkShell {
-      buildInputs = [ self.defaultPackage.x86_64-linux ];
-    };
-
-    defaultPackage.x86_64-linux = nixpkgs.stdenv.mkDerivation {
-      pname = "parasoft-cpptest";
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    packages.${system}.default = pkgs.stdenv.mkDerivation {
+      pname = "parasoft-cpptestcli";
       version = "2024.1.0";
 
-      src = nixpkgs.fetchurl {
+      src = pkgs.fetchurl {
         url = "ftp://10.0.1.162/upload/parasoft_cpptest_standard-2024.1.0-linux.x86_64.rekovar.tar.gz";
         sha256 = "0r3c377g63f2yb37cviwscdgsqk8c5p0h7j13p65x14vjqi9nd1k";
       };
 
-      buildInputs = [ nixpkgs.libarchive ];
+      buildInputs = [ pkgs.libarchive ];
 
       installPhase = ''
         mkdir -p $out/bin
@@ -28,13 +29,22 @@
         ln -s $out/cpptestcli $out/bin/cpptestcli
       '';
 
-      meta = with nixpkgs.lib; {
-        description = "Parasoft C++test Standard Edition";
-        homepage = "https://www.parasoft.com/products/ctest/";
-        license = licenses.proprietary;
-        maintainers = with maintainers; [ "abelREK" ];
+      meta = with pkgs.lib; {
+        description = "Parasoft CppTestCli";
+        homepage = "https://example.com";
+        license = licenses.mit;
+        platforms = platforms.linux;
+        maintainers = with maintainers; [ abeljim ];
       };
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        self.packages.${system}.default
+      ];
+      # shellHook = ''
+      #   echo "Started Parasoft CppTestCli environment..."
+      # '';
     };
   };
 }
-
