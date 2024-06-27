9563b2d83d5403fbe3af642552af703c7c25c218
Abel Jimenez <abel.jimenez@rekovar.com>
2024-06-26 23:00:02 -0700

    updated hashes

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
 
   src = pkgs.fetchurl {
     url = "ftp://10.0.1.162/upload/parasoft_cpptest_standard-2024.1.0-linux.x86_64.rekovar.tar.gz";
-    sha256 = "0r3c377g63f2yb37cviwscdgsqk8c5p0h7j13p65x14vjqi9nd1k";
+    sha256 = "0g6qiy7gda17as6f9vi146bpq6gvxi96l8g04kibzp7lvhaxax94";
   };
 
   buildInputs = [ pkgs.libarchive ];
